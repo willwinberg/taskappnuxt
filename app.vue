437d38e43a0207d40774500fb53cd45c9d06d371
William <wtpwinberg@gmail.com>
2024-01-05 09:46:13 -0700

    init

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+useState('tasks', () => [
+  {
+    id: 1,
+    title: 'First Task',
+    description: 'The first one'
+  }
+])
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
---
+++ pages/tasks/[id]/update.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  validate: async (route) => {
+    // Check if the id is made up of digits
+    return /^\d+$/.test(route.params.id)
+  }
+})
+
+const route = useRoute()
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
+
+<template>
+    <div>update task {{ route.params.id }}</div>
+</template>
