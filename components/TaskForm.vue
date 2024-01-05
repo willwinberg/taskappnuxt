56e3e3aed6ae5dcb3759eefd06ee9d30128d715c
William <wtpwinberg@gmail.com>
2024-01-05 16:25:26 -0700

    input margin fix

--- components/TaskForm.vue
+++ components/TaskForm.vue
@@ -67,7 +67,7 @@ const deleteTask = () => {
             >Title</label
           >
           <div
-            class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+            class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
           >
             <input
               type="text"
